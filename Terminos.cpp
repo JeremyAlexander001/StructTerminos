6a9cc3d9d12be2ab76f5871191bb11500b7b6298
Jeremy Alexander <jeremy.chancahuana.t@uni.pe>
2025-06-05 11:22:27 -0500

    Codigo Terminos.cpp

---
+++ Terminos.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+struct Termino{
+    float Coef;
+    int Expo;
+};
+int main(){
+    cout<<"hello wordl"<<endl;
+    Termino Polinomio;
+    Polinomio.Coef=1;
+    Polinomio.Expo=1;
+    return 0;
+}
